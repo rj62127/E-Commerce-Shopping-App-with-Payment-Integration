683b6bc9fa4214cf79deb08eeeacf7e9fc939eb7
rj62127 <rjha2.718@gmail.com>
2024-02-16 18:28:51 +0530

    update view

--- onlinex/app/views.py
+++ onlinex/app/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Customer, Product, Cart, OrderPlaced
 
-def home(request):
- return render(request, 'app/home.html')
+# def home(request):
+#  return render(request, 'app/home.html')
+class ProductView(View):
+ def get(self, request):
+  topwears = Product.objects.filter(category='TW')
+  bottomwears = Product.objects.filter(category='BW')
+  mobile = Product.objects.filter(category='M')
+  laptop = Product.objects.filter(category='L')
+
+  return render(request, 'app/home.html', {'topwears':topwears, 'bottomwears':bottomwears, 'mobile':mobile, 'laaptop':laptop})
+
+  
 
 def product_detail(request):
  return render(request, 'app/productdetail.html')
