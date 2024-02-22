bb302b392ca8dce347c27615fce3b859b9df8b5d
rj62127 <rjha2.718@gmail.com>
2024-02-22 14:10:51 +0530

    Update view for filter

--- onlinex/app/views.py
+++ onlinex/app/views.py
@@ -42,29 +42,70 @@ def change_password(request):
  return render(request, 'app/changepassword.html')
 
 def mobile(request, data=None):
- if data == None:
-  mobiles  = Product.objects.filter(category='M')
- elif data == 'Redmi' or data == 'Samsung' or data == 'Apple' or data == 'Motrola' or data == 'Poco' or data == 'redmi':
-  mobiles  = Product.objects.filter(category='M').filter(brand=data)
- elif data == 'below':
-  mobiles  = Product.objects.filter(category='M').filter(discounted_price__lt=10000)
- elif data == 'above':
-  mobiles  = Product.objects.filter(category='M').filter(discounted_price__gt=10000)
+    if data is None:
+        mobiles = Product.objects.filter(category='M')
+    elif data in ['Redmi', 'Samsung', 'Apple', 'Motrola', 'Poco', 'redmi']:
+        mobiles = Product.objects.filter(category='M', brand=data)
+    elif data == 'below':
+        mobiles = Product.objects.filter(category='M', discounted_price__lt=10000)
+    elif data == 'above':
+        mobiles = Product.objects.filter(category='M', discounted_price__gt=10000)
+    else:
+        # Handle invalid data parameter, maybe return an empty queryset or raise an error
+        mobiles = Product.objects.none()
 
- return render(request, 'app/mobile.html', {'mobiles': mobiles})
+    return render(request, 'app/mobile.html', {'mobiles': mobiles})
+
 
 
 def laptop(request, data=None):
- if data == None:
-  laptops  = Product.objects.filter(category='L')
- elif data == 'HP' or data == 'Samsung' or data == 'Apple' or data == 'Dell' or data == 'Asus' or data == 'redmi':
-  laptops  = Product.objects.filter(category='L').filter(brand=data)
- elif data == 'below':
-  laptops  = Product.objects.filter(category='L').filter(discounted_price__lt=30000)
- elif data == 'above':
-  laptops  = Product.objects.filter(category='L').filter(discounted_price__gt=30000)
+    if data is None:
+        laptops = Product.objects.filter(category='L')
+    elif data in ['HP', 'Samsung', 'Apple', 'Dell', 'Asus', 'redmi']:
+        laptops = Product.objects.filter(category='L', brand=data)
+    elif data == 'below':
+        laptops = Product.objects.filter(category='L', discounted_price__lt=30000)
+    elif data == 'above':
+        laptops = Product.objects.filter(category='L', discounted_price__gt=30000)
+    else:
+        # Handle invalid data parameter, maybe return an empty queryset or raise an error
+        laptops = Product.objects.none()
 
- return render(request, 'app/laptop.html', {'laptops': laptops})
+    return render(request, 'app/laptop.html', {'laptops': laptops})
+
+
+def topwear(request, data=None):
+    if data is None:
+        topwears = Product.objects.filter(category='TW')
+    elif data in ['Lee', 'Jockey', 'Copper', 'HRX', 'XHR', 'Discy', 'Allen']:
+        topwears = Product.objects.filter(category='TW', brand=data)
+    elif data == 'below':
+        topwears = Product.objects.filter(category='TW', discounted_price__lt=1000)
+    elif data == 'above':
+        topwears = Product.objects.filter(category='TW', discounted_price__gt=1000)
+    else:
+        # Handle invalid data parameter, maybe return an empty queryset or raise an error
+        topwears = Product.objects.none()
+
+    return render(request, 'app/topwear.html', {'topwears': topwears})
+
+
+
+def bottomwear(request, data=None):
+    if data is None:
+        bottomwears = Product.objects.filter(category='BW')
+    elif data in ['Lee', 'XHR','Allen', 'Spyker']:
+        bottomwears = Product.objects.filter(category='BW', brand=data)
+    elif data == 'below':
+        bottomwears = Product.objects.filter(category='BW', discounted_price__lt=1000)
+    elif data == 'above':
+        bottomwears = Product.objects.filter(category='BW', discounted_price__gt=1000)
+    else:
+        # Handle invalid data parameter, maybe return an empty queryset or raise an error
+        bottomwears = Product.objects.none()
+
+    return render(request, 'app/bottomwear.html', {'bottomwears': bottomwears})
+
 
 def login(request):
  return render(request, 'app/login.html')
